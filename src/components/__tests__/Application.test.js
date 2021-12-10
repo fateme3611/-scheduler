ba905aa3c2d44e9dde39c65abcffc630b57f92ad
fateme <fateme3611@yahoo.com>
2021-12-10 14:13:15 -0500

    replace promise in test file with async method

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -14,11 +14,10 @@ it("Defaults to Monday and changes the schedule when a new day is selected", () 
 
 });
 
-it("Change day to Tuesday and changes the schedule when a new day is selected", () => {
+it("Change day to Tuesday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  await waitForElement(() => getByText("Monday"))
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
