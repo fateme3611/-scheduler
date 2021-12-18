fecb8ce4710fa21890519fc2014207467b9c2dd4
fateme <fateme3611@yahoo.com>
2021-12-18 16:20:11 -0500

    add intrviewer validation message

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,25 +7,37 @@ export default function Form(props) {
 
     const [name, setName] = useState(props.name || "");
     const [interviewer, setInterviewer] = useState((props.interviewer && props.interviewer.id) || null);
-    const [canContinue, setCanContinue] = useState(true);
+    const [errMsg, setErrMsg] = useState("");
+
+
     const reset = () => {
         setName('');
-
-        setInterviewer(null)
+        setInterviewer(null);
+        setErrMsg('');
     }
     const cancel = () => {
         reset();
         props.onCancel();
     }
 
+    const validate = () => {
+        let res = <section className="appointment__validation">
+            {!name && <div>Student name cannot be blank <span aria-label="" role="img">👆</span></div>
+            }
+            {!interviewer &&
+                <div>Please select an interviewer <span aria-label="" role="img">👇</span></div>
+            }
+        </section>
+        setErrMsg(res);
+    }
+
     const onSave = () => {
-        setCanContinue(name && interviewer);
+
+        validate();
         if (name && interviewer) {
             props.onSave(name, interviewer);
         }
     }
-
-    const noStudentErr = canContinue ? "" : "student name cannot be blank";
 
     return (
         <main className="appointment__card appointment__card--create">
@@ -38,12 +50,10 @@ export default function Form(props) {
                         name="name"
                         type="text"
                         placeholder="Enter Student Name"
-                        onChange={event => { setName(event.target.value); setCanContinue(true); }}
+                        onChange={event => setName(event.target.value)}
 
                     />
-                    {!name &&
-                        <section className="appointment__validation">{noStudentErr}</section>
-                    }
+                    {errMsg}
                 </form>
                 <InterviewerList onChange={(id) => setInterviewer(id)} interviewer={interviewer} interviewers={props.interviewers}
                 /* your code goes here */
